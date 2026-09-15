0ddcb34e6e3ba53f74c407b8ca81461c6dab2568
Elgato91169 <syarofulhaidar7@gmail.com>
2026-09-16 00:51:25 +0700

    Delete Pertemuan 2 directory

--- Pertemuan 2/TugasMandiri2_OOP_225443051_SyarofulHaidarS/src/Format.kt
+++
@@ -1,6 +0,0 @@
-import java.util.Locale
-
-/**
- * Mengubah nominal angka menjadi teks rupiah tanpa angka pecahan.
- */
-fun formatRupiah(amount: Double): String = String.format(Locale.forLanguageTag("id-ID"), "Rp%,.0f", amount)
